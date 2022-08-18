2f12aa49548297c662ee5b138267568956527549
Scottie Blangeres <scottblangeres13@gmail.com>
2022-08-18 05:43:01 -0700

    Changed to check if fires are from past year, added info window when you click on fire pins

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,6 +1,15 @@
+var date = new Date();
+var year = date.getYear();
+console.log(year)
+
+function yearFormat(a){
+    var yearNum = Number(a) - 1900;
+    return yearNum;
+}
+
 function makeMap(){
     //Getting the NASA api
-    fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events")
+    fetch("https://eonet.gsfc.nasa.gov/api/v3/events")
     .then(res => res.json())
     .then(data => {
         //Creating the map
@@ -17,17 +26,31 @@ function makeMap(){
         //Adding the fire locations to map
         var eventList = data.events;
         eventList.forEach(element => {
-            //Checking if event is a wildfire
-            if(element.categories[0].id === 8){
+            var eventYear = element.geometry[0].date
+            //Checking if event is a wildfire and from this year, then running newItem() if true
+            if(element.categories[0].id === 'wildfires' && yearFormat(element.geometry[0].date.split('-')[0]) == year){
+                newItem();
+            }
+            //Function to create markers for every event in nasa api
+            function newItem(){
+                var contentString = '<h3>' + element.title + '</h3>' + '<h5>' + 'Sources: ' + '</h5>' + '<a href=' + element.sources[0].url + '>' + element.sources[0].url + '</a>' + '<p>' + element.geometry[0].date + '</p>';
                 var marker = new google.maps.Marker({
-                    position: {lat: element.geometries[0].coordinates[1], lng: element.geometries[0].coordinates[0]},
+                    position: {lat: element.geometry[0].coordinates[1], lng: element.geometry[0].coordinates[0]},
                     map: map,
                     icon: icon
+                });
+                //Options for info window
+                var infoWindowOptions = {
+                    content: contentString,
+                    map: map,
+                    anchor: marker
+                }
+                //Creating the info window when we click on a fire pin
+                google.maps.event.addDomListener(marker, 'click', function(){
+                    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);   
                 })
             }
-        });
-
-        
+        }); 
     });
 }
  
